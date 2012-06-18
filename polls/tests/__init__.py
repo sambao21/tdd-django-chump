f8dedef30aad8c089098b96511b76e5cf17a5026
Sam Bao <sambao21@gmail.com>
2012-06-17 21:58:07 -0500

    finish part5

---
+++ polls/tests/__init__.py
@@ -0,0 +1,3 @@
+from polls.tests.tests import *
+from polls.tests.test_models import *
+from polls.tests.test_views import *
--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
-from polls.models import Poll
+from polls.models import Poll, Choice
 from polls.forms import PollVoteForm
+from django.core.urlresolvers import reverse
+from django.http import HttpResponseRedirect
 
 
 def home(request):
@@ -9,6 +11,12 @@ def home(request):
 
 
 def poll(request, poll_id):
+    if request.method == 'POST':
+        choice = Choice.objects.get(id=request.POST['vote'])
+        choice.votes += 1
+        choice.save()
+        return HttpResponseRedirect(reverse('polls.views.poll', args=[poll_id, ]))
+
     poll = Poll.objects.get(pk=poll_id)
     form = PollVoteForm(poll=poll)
     return render(request, 'poll.html', {'poll': poll, 'form': form})
